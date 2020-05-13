8aaff385df7681a125f2b8d739b53911e05cd33e
Patryk Lipka <papec656@gmail.com>
2020-05-13 11:50:53 +0200

    Chyba naprawiony glock.h

--- Engine/Glock.h
+++ Engine/Glock.h
@@ -5,9 +5,11 @@ class Glock :
 {
 	std::vector<Vec2D> possibleShot;
 public:
-	Glock(int neededPoints, float damage, float rof, float distance, float spread, int ammo) :Weapon(neededPoints, damage, rof, distance, spread, ammo){}
+	Glock(int neededPoints, float damage, float rof, float distance, float spread, int ammo, float holdTime) :Weapon(neededPoints, damage, rof, distance, spread, ammo, holdTime){}
 	void calculatePossibleShot(bool aimsRight, bool aimsLeft, bool aimsDown, bool aimsUp,const Vec2D& pos);
-	int Shoot(bool aimsRight, bool aimsLeft, bool aimsDown, bool aimsUp,std::vector<Enemy*> enemy, const Vec2D& pos)override;
+	bool Shoot(bool aimsRight, bool aimsLeft, bool aimsDown, bool aimsUp, std::vector<Enemy*> enemy, std::vector<Obstacle>obstacles, const Vec2D& pos, float dt)override;
+	void DrawShot(Graphics& gfx, bool aimsRight, bool aimsLeft, bool aimsDown, bool aimsUp, int distance, const Vec2D& pos) override;
+	bool CheckIfObstacleCanBeHitted(Obstacle obstacle);
 	bool CheckIfEnemyCanBeHitted(Enemy* enemy);
 	void CleanVector();
 };
