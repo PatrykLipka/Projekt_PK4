597e623834a31e00464467ef3fc38b2fcebece0b
Patryk Lipka <papec656@gmail.com>
2020-05-18 15:55:48 +0200

    Scalone

--- Engine/Round.cpp
+++ Engine/Round.cpp
@@ -74,7 +74,7 @@ void Round::SpawnEnemy(float dt)
 			if (x >= 700)break;
 		}
 		if (findPosition == true) {
-			enemies.push_back(std::make_unique<Zombie>(obj, 100, 50, true, 5));
+			enemies.push_back(std::make_unique<Zombie>(obj, 100,20, 50, true, 5));
 			currentTime = 0;
 			--numberOfEnemiesToSpawn;
 		}
