6a359b91c61f326ef53fb81fc4d1f0b670e14971
Patryk Lipka <papec656@gmail.com>
2020-05-29 21:05:02 +0200

    Zmiany balansowe

--- Engine/Sharprshooter.h
+++ Engine/Sharprshooter.h
@@ -5,7 +5,7 @@ class Sharpshooter :
 {
 	std::vector<Vec2D> possibleShot;
 public:
-	Sharpshooter(int neededPoints = 50, float damage = 30, float rof = 2, float distance = 220, float spread = 1, int ammo = 10, float holdTime = 1.5f, float holdTimeOfAnimation = 0.09f) :Weapon(neededPoints, damage, rof, distance, spread, ammo, holdTime, holdTimeOfAnimation) {}
+	Sharpshooter(int neededPoints = 50, float damage = 80, float rof = 3, float distance = 450, float spread = 1, int ammo = 10, float holdTime = 1.5f, float holdTimeOfAnimation = 0.09f) :Weapon(neededPoints, damage, rof, distance, spread, ammo, holdTime, holdTimeOfAnimation) {}
 	void calculatePossibleShot(bool aimsRight, bool aimsLeft, bool aimsDown, bool aimsUp, const Vec2D& pos);
 	bool Shoot(bool aimsRight, bool aimsLeft, bool aimsDown, bool aimsUp, std::vector<std::unique_ptr<Enemy>>& enemies, std::vector<Obstacle>obstacles, const Vec2D& pos, float dt, bool isMoving)override;
 	bool DrawShot(Graphics& gfx, float dt)override;
